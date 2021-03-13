ab6b9eadd08aae26047645421be7bb82561fbe11
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2021-03-13 14:16:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 from colorama import Fore, Back, Style
 import config 
 from telethon import TelegramClient, sync
-
+#config.py
 api_id = config.api_id()
 api_hash = congig.api_hash()
-
+#start client
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
