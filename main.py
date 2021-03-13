9edf374df1d6b9e814f1cffd25f037a832d229b9
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2021-03-13 14:11:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #new kernel
+from telethon import TelegramClient, sync
+
+api_id = 12345
+api_hash = '0123456789abcdef0123456789abcdef'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
