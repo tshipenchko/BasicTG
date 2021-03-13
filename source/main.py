d4fe693510eca9bb5fdfa46be3c373a80334340d
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2021-03-13 14:23:24 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -4,7 +4,7 @@ import config
 from telethon import TelegramClient, sync
 #config.py
 api_id = config.api_id()
-api_hash = congig.api_hash()
+api_hash = config.api_hash()
 #start client
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
