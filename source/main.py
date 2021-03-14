8db75e5aa203d2a511ce272b5dc851623ee49ca6
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2021-03-14 18:06:25 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -2,9 +2,14 @@
 from colorama import Fore, Back, Style
 import config 
 from telethon import TelegramClient, sync
+from telethon import events
 #config.py
 api_id = config.api_id()
 api_hash = config.api_hash()
 #start client
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
+@client.on(events.NewMessage(pattern='(?i).*ping'))
+async def handler(event):
+	await event.edit('Pong!')
+client.run_until_disconnected()
