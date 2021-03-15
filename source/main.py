7e04531cc3edf209d81b7c975e877d4c50b10fe2
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2021-03-15 20:28:00 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -5,26 +5,27 @@ from telethon import TelegramClient, sync
 from telethon import events
 import time
 from random import randint
+import os 
+#import modules
+
 #config.py
 api_id = config.api_id()
 api_hash = config.api_hash()
 #start client
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
-@client.on(events.NewMessage(pattern='(?i).*ping'))
+@client.on(events.NewMessage(pattern='(?i).*restart'))
 async def handler(event):
-	await event.edit('Pong!')
-@client.on(events.NewMessage(pattern='(?i).*fl'))
+	try:
+		await event.edit(f'Restarting... ')
+		time.sleep(randint(1,2))
+		await event.edit(f'Restart succefully!')
+		os.abort()
+	except:
+		await event.edit(f'Restart failed!')
+import modules
+@client.on(events.NewMessage(pattern='(?i).*install'))
 async def handler(event):
-	a = 0
-	while a <= 100:
+	return 
 
-		await event.edit(f'Loading {a}%')
-		a +=1
-	time.sleep(randint(1,2))
-	await event.edit('Done!')
-@client.on(events.NewMessage(pattern='(?i).*gay'))
-async def handler(event):
-	p = randint(0,100)
-	await event.edit(f'🏳️‍🌈 I am {str(p)}% gay.')
 client.run_until_disconnected()
