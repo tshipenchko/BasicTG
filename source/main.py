4b53abe56e645dce2f04da6f80370ab125982e2f
Arsen Smerechuk <71315641+arersen@users.noreply.github.com>
2021-03-16 21:37:30 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -6,7 +6,7 @@ from telethon import events
 import time
 from random import randint
 import os 
-#import modules
+import var
 
 #config.py
 api_id = config.api_id()
@@ -14,7 +14,6 @@ api_hash = config.api_hash()
 #start client
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
-
 
 
 @client.on(events.NewMessage(pattern='(?i).*restart'))
@@ -30,25 +29,27 @@ async def handler(event):
 @client.on(events.NewMessage(pattern='(?i).*install'))
 async def handler(event):
 	replied = await event.get_reply_message()
-	module_text = 'hello world!'
+	await client.download_media(replied, 'module')
+	file_m = open('module.py','r')
+	module_text = file_m.read()
+	file_m.close()
 	file = open('main.py','r')
 	file_read = file.read()
-	text = file_read.replace('','')
+	text = file_read.replace(var.crud,'')
 	file.close()
 	file = open('main.py','w')
 	file.write(text)
 	file.close()
 	file = open('main.py','a')
-	file.write(module_text)
-	file.write('\n')
-	await event.respond(f' {replied}')
+	file.write(f'\n{module_text}\n\n{var.crud}')
+	
+	time.sleep(1)
 	await event.edit('Module installed!')
-	os.abort()
+	
 
 @client.on(events.NewMessage(pattern='(?i).*ping'))
 async def handler(event):
 
 	await event.edit('Pong!')
-
 
 client.run_until_disconnected()
