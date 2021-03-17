b12c469500d9a90c56ac264e8993dab979fa8a90
tshipenchko <52406854+tshipenchko@users.noreply.github.com>
2021-03-17 10:46:01 +0600

    Automatic installation with one command
    Use 
    ```bash
    bash <(wget -qO- https://raw.githubusercontent.com/arersen/BasicTG/main/install.sh)
    ```
    OR
    ```bash
    bash <(curl -Ls https://raw.githubusercontent.com/arersen/BasicTG/main/install.sh)
    ```

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+cd ~ 
+if [ -d "BasicTG" ]; then
+  echo "The directory already exists. Please delete the directory to install it completely OR update the repository"
+  echo "Run        rm -rf ~/BasicTG             to delete the directory OR"
+  echo "Run        cd ~/BasicTG && git pull     to update the repository"
+  exit 1
+fi
 
-apt update && apt upgrade
-apt install python
+apt update && apt upgrade -y
+apt install python -y
 pip install telethon
 pip install colorama
-apt install git
+apt install git -y
+git clone https://github.com/arersen/BasicTG
+cd BasicTG
 cd source
 python install.py
-cd .. 
+cd ..
 sh start.sh
-
